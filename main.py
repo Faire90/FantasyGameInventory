f1ee82508176a146e7dcfe7bcec0d9b4ab1d38c3
Faire90 <Faire90@users.noreply.github.com>
2021-04-13 18:20:38 -0700

    Added solution to the next practice project
    The next practice project asks to use a list to add items and values to a dictionary.  The "addToInventory" function provides a solution to this next practice project.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pprint
 
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 #function for displaying inventory
 def displayInventory(inventory):
     print("Inventory:")
@@ -7,10 +9,23 @@ def displayInventory(inventory):
     #iterate through the given dictionary totaling all quantities (v)
     #and displaying each item (k) with its number (v)
     for k, v in inventory.items():
-        item_total = item_total + v
+        item_total += v
         print(k, v)
     pprint.pprint("Total number of items: " + str(item_total))
 
+def addToInventory(inventory, addedItems):
+#iterate through the added items list.
+#With each item in the list, find the item in the original inventory and add one.
+#if the item doesn't exist, then add the item and set it's total to 1.
+    for i in addedItems:
+        inventoryAddition = inventory.get(i, 0)
+        if inventoryAddition != 0:
+            inventory[i] = inventoryAddition + 1
+        else:
+            inventory[i] = 1
+
+
 if __name__ == '__main__':
-    stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+    displayInventory(stuff)
+    addToInventory(stuff, dragonLoot)
     displayInventory(stuff)
